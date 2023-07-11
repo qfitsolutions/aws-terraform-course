e234ebc094136649bcde7ea52cd3cfc4ff7b695f
Ravi <ravi@vytec.com>
2023-07-11 08:56:30 +0530

    updated

--- example/modules/eks/main.tf
+++ example/modules/eks/main.tf
@@ -1,9 +1,9 @@
 resource "aws_eks_cluster" "example" {
   name     = "example"
-  role_arn = aws_iam_role.example.arn
+  role_arn = var.rolearn
 
   vpc_config {
-    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+    subnet_ids = [subnet-62c32548, subnet-9fcc53e7]
   }
  depends_on = [
     aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
---
+++ example/modules/eks/variables.tf
@@ -0,0 +1,3 @@
+variable "rolearn"{
+    type = string
+}
