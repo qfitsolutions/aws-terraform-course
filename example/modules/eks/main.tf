d992441af710261b059ea5a0a36700503cc317a6
Ravi <ravi@vytec.com>
2023-07-11 08:44:32 +0530

    updated

---
+++ example/modules/eks/main.tf
@@ -0,0 +1,20 @@
+resource "aws_eks_cluster" "example" {
+  name     = "example"
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+  }
+ depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
--- example/mysite.tf
+++ example/mysite.tf
@@ -1,4 +1,12 @@
-module "abc"{
+module "ec2"{
     source = "./modules/ec2"
+    instancetype = "t2.small"
+}
+module "ecs"{
+    source = "./modules/ecs"
+    instancetype = "t2.small"
+}
+module "eks"{
+    source = "./modules/eks"
     instancetype = "t2.small"
 }
