7b419e633f2167967052c669e1e322a4f4d9d44d
Ravi <ravi@vytec.com>
2023-07-11 09:04:13 +0530

    uodwted

--- example/mysite.tf
+++ example/mysite.tf
@@ -2,10 +2,10 @@ module "ec2"{
     source = "./modules/ec2"
     instancetype = "t2.small"
 }
-module "ecs"{
-    source = "./modules/ecs"
-}
-module "eks"{
-    source = "./modules/eks"
-    rolearn = "arn:aws:iam::675467602881:role/eksClusterRole"
-}
+# module "ecs"{
+#     source = "./modules/ecs"
+# }
+# module "eks"{
+#     source = "./modules/eks"
+#     rolearn = "arn:aws:iam::675467602881:role/eksClusterRole"
+# }
