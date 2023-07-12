2e3c920e7c0cded977209d12b622867387cd5905
Ravi <ravi@vytec.com>
2023-07-12 08:35:24 +0530

    updtwdd

--- example/modules/s3/main.tf
+++ example/modules/s3/main.tf
@@ -2,10 +2,10 @@ variable "bucket_list" {
   type = list
   default  = ["demo-ck-1120720235656", "demo-ck-21120720235656", "demo-ck-31120720235656"]
 }
-resource "aws_s3_bucket" "demo_bucket" {
-  count = length(var.bucket_list)
-  bucket = var.bucket_list[count.index]
-}
+# resource "aws_s3_bucket" "demo_bucket" {
+#   count = length(var.bucket_list)
+#   bucket = var.bucket_list[count.index]
+# }
 
 resource "aws_s3_bucket" "demo_bucket" {
   for_each = toset(var.bucket_list)
