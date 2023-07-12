4788998bb20e96713d280c21791e8ba314c7c9e5
Ravi <ravi@vytec.com>
2023-07-12 08:46:59 +0530

    updated

--- example/modules/s3/exc2.tf
+++ example/modules/s3/exc2.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-west-2"
-}
-
 resource "aws_instance" "app_server" {
   for_each = var.ec2s
   ami           = each.value["ami"] 
